c74c7b51ec5231270e991992654375fdb393ce25
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2019-06-08 17:06:21 +0000

    Updated the config

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI:
+    "mongodb://blog_user:blog_89_03@mlazlab-shard-00-00-zz2cq.mongodb.net:27017,mlazlab-shard-00-01-zz2cq.mongodb.net:27017,mlazlab-shard-00-02-zz2cq.mongodb.net:27017/blog_app?ssl=true&replicaSet=mlazlab-shard-0&authSource=admin&retryWrites=true",
+  cookieKey: "123123123"
 };
